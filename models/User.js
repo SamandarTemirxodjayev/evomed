e612d00f70e9061990f7a6b5e525487704f4bae8
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-05-18 23:43:37 +0500

    first commit

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String
+  },
+  type: {
+    type: String
+  },
+});
+const Users = mongoose.model('users', userSchema);
+
+module.exports = Users
